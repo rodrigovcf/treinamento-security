b9da6ac194855825a774babaec20377f895f2053
rodrigovcf <rodrigo.rvcf@gmail.com>
2022-08-17 19:55:25 -0300

    Configuração Inicial Básica

---
+++ treinamento-security/src/main/java/com/treinamentosecurity/TreinamentoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoSecurityApplication.class, args);
+	}
+
+}
---
+++ treinamento-security/src/test/java/com/treinamentosecurity/TreinamentoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
